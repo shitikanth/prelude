a3309a63856680bc52c55f53501e2c867d0c02d1
Shitikanth Kashyap <shitikanth1@gmail.com>
2021-01-19 16:30:12 +0530

    Set user-emacs-directory to prelude-dir

--- init.el
+++ init.el
@@ -54,6 +54,7 @@
 ;; Define Prelude's directory structure
 (defvar prelude-dir (file-name-directory load-file-name)
   "The root dir of the Emacs Prelude distribution.")
+(setq user-emacs-directory prelude-dir)
 (defvar prelude-core-dir (expand-file-name "core" prelude-dir)
   "The home of Prelude's core functionality.")
 (defvar prelude-modules-dir (expand-file-name  "modules" prelude-dir)
